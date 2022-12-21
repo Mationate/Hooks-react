9a1fd01095b3a68e5f8706b1d60441dd92644da5
Matias Onate <mationate.0@gmail.com>
2022-12-20 21:28:53 -0300

    TODO list, seccion terminada

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { FormWithCustomHook } from './02-useEffect/FormWIthCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
-import { CallbackHook } from './06-memos/CallbackHook'
-import { MemoHook } from './06-memos/MemoHook'
-import { Memorize } from './06-memos/Memorize'
-import { Padre } from './07-tarea-memo/Padre'
-import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { FormWithCustomHook } from './02-useEffect/FormWIthCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { CallbackHook } from './06-memos/CallbackHook'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { Memorize } from './06-memos/Memorize'
+// import { Padre } from './07-tarea-memo/Padre'
+// import { HooksApp } from './HooksApp'
+// import './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
 import './index.css'
-import './08-useReducer/intro-reducer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
@@ -21,5 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // <FocusScreen />
     // <MemoHook/>
         // <CallbackHook/>
-        <Padre/>
+        // <Padre/>
+        <TodoApp />
 )
